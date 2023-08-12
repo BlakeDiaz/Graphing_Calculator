6a1983d7f0ffa2c3b7aa10b635c262ace40f802a
BlakeDiaz <176447188+BlakeDiaz@users.noreply.github.com>
2023-08-11 23:43:18 -0700

    refactor: Got rid of redundant Calculator variable
    Got rid of unused user_function_map variable in the Calculator class.

--- include/Calculator.hpp
+++ include/Calculator.hpp
@@ -13,8 +13,6 @@ class Calculator
 {
 public:
     const static std::unordered_map<std::string, Function> function_map;    // Contains functions like sin, cos, etc.
-    static std::unordered_map<char, UserFunction> user_function_map;    // Stores user-defined functions
-
     
     static std::tuple<std::string, std::optional<UserFunction>> format_expression(std::unordered_map<char, UserFunction>& user_function_map, std::string expression);
     static double solve_expression(std::string formatted_expression);
