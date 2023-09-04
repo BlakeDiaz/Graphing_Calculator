ea5f38aa2b5c7c2e9c177fd0197309185f650edf
BlakeDiaz <176447188+BlakeDiaz@users.noreply.github.com>
2023-09-04 16:35:05 -0700

    test: Added test for dependency locator

--- src/calculator_test.cc
+++ src/calculator_test.cc
@@ -44,4 +44,23 @@ TEST_F(CalculatorTestFixture, FormatExpression)
     EXPECT_STREQ(Calculator::format_expression(user_function_map, "g(x) = 5x + 4").c_str(), "5*(x)+4");
 }
 
+TEST_F(CalculatorTestFixture, LocateUserFunctionDependencies)
+{
+    user_function_map.emplace('a', UserFunction("a(x) = 3x", "3*(x)", {}));
+    user_function_map.emplace('b', UserFunction("b(x) = 3a(x)", "3*3*((x))", {'a'}));
+    user_function_map.emplace('c', UserFunction("c(x) = 3b(x)", "3*3*3*(((x)))", {'a', 'b'}));
+    user_function_map.emplace('d', UserFunction("d(x) = 3c(x)", "3*3*3*3*((((x))))", {'a', 'b', 'c'}));
+    user_function_map.emplace('e', UserFunction("e(x) = 3d(x)", "3*3*3*3*3*(((((x)))))", {'a', 'b', 'c', 'd'}));
+
+    for (char c = 'a'; c < 'f'; c++)
+    {
+        UserFunction& user_function = user_function_map.at(c);
+        EXPECT_EQ(user_function.user_function_dependencies.size(),
+                  Calculator::locate_user_function_dependencies(user_function_map, user_function.expression).size());
+    }
+
+    EXPECT_EQ(Calculator::locate_user_function_dependencies(user_function_map, "g(x) = 4c(x) + 2^a(x)").size(), 3);
+    EXPECT_EQ(Calculator::locate_user_function_dependencies(user_function_map, "g(x) = 4c(x) + 2^a(x) + f(x)").size(), 4);
+}
+
 }
