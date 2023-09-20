c30a139a8dc68fef9fcfaf79dfb8731abd144537
BlakeDiaz <176447188+BlakeDiaz@users.noreply.github.com>
2023-09-19 17:11:11 -0700

    refactor: Remove process input function
    Removed unused process_input function from Input_Manager.

--- include/Input_Manager.hpp
+++ include/Input_Manager.hpp
@@ -8,8 +8,4 @@ class Input_Manager
 {
 public:
     static std::unordered_map<char, User_Function> create_user_function_map(std::vector<std::tuple<std::string, std::unordered_set<char>>>& new_user_function_expressions);
-    static std::optional<std::string> process_input(std::unordered_map<char, User_Function>& user_function_map, std::vector<std::unordered_set<char>>& user_functions, std::string expression);
-private:
-    static void process_user_function_input(std::unordered_map<char, User_Function>& user_function_map, std::vector<std::unordered_set<char>>& user_functions, User_Function& user_function);
-    static void add_user_function(std::unordered_map<char, User_Function>& user_function_map, std::vector<std::unordered_set<char>>& user_functions, User_Function& user_function);
 };
